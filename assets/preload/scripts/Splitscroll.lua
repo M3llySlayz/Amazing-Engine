7aced7a0b6acd6cf80c3123f470ea7f1bac90f7d
IrshaadAli1 <98787249+IrshaadAli1@users.noreply.github.com>
2023-05-28 03:16:11 -0400

    Small Fix Splitscroll.lua

--- assets/preload/scripts/Splitscroll.lua
+++ assets/preload/scripts/Splitscroll.lua
@@ -8,10 +8,7 @@ function math.round(val)
 end
 --This is needed for rounding variables clearly.
 --Also now all keys should be supported
---it's 10:51 PM as i'm writing this. -Irshaad
-
---Changelog 27/05/2023
---so i just added if value is null to the list here, don't mind me -Irshaad
+--it's 10:51 as i'm writing this. -Irshaad
 
 local on
 local alt
@@ -40,9 +37,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true) 
                 end
-                for i = 1,1 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
-                end
             end
             if mania == 3 then
                 for i = 0,1 do
@@ -54,9 +48,6 @@ function onCreatePost()
                 for i = 0,1 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true) 
-                end
-                for i = 2,2 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
                 end
             end
             if mania == 5 then
@@ -70,9 +61,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true) 
                 end
-                for i = 3,3 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
-                end
             end
             if mania == 7 then
                 for i = 0,3 do
@@ -84,9 +72,6 @@ function onCreatePost()
                 for i = 0,3 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true)
-                end
-                for i = 4,4 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
                 end
             end
             if mania == 9 then
@@ -100,9 +85,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true)
                 end
-                for i = 5,5 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
-                end
             end
             if mania == 11 then
                 for i = 0,5 do
@@ -114,9 +96,6 @@ function onCreatePost()
                 for i = 0,5 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true)
-                end
-                for i = 6,6 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
                 end
             end
             if mania == 13 then
@@ -130,9 +109,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true)
                 end
-                for i = 7,7 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
-                end
             end
             if mania == 15 then
                 for i = 0,7 do
@@ -144,9 +120,6 @@ function onCreatePost()
                 for i = 0,7 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',true)
-                end
-                for i = 8,8 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 250))
                 end
             end
             if mania == 17 then
@@ -177,9 +150,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false) 
                 end
-                for i = 1,1 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
-                end
             end
             if mania == 3 then
                 for i = 0,1 do
@@ -191,9 +161,6 @@ function onCreatePost()
                 for i = 0,1 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false) 
-                end
-                for i = 2,2 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
                 end
             end
             if mania == 5 then
@@ -207,9 +174,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false) 
                 end
-                for i = 3,3 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
-                end
             end
             if mania == 7 then
                 for i = 0,3 do
@@ -221,9 +185,6 @@ function onCreatePost()
                 for i = 0,3 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false)
-                end
-                for i = 4,4 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
                 end
             end
             if mania == 9 then
@@ -237,9 +198,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false)
                 end
-                for i = 5,5 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
-                end
             end
             if mania == 11 then
                 for i = 0,5 do
@@ -251,9 +209,6 @@ function onCreatePost()
                 for i = 0,5 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false)
-                end
-                for i = 6,6 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
                 end
             end
             if mania == 13 then
@@ -267,9 +222,6 @@ function onCreatePost()
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false)
                 end
-                for i = 7,7 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
-                end
             end
             if mania == 15 then
                 for i = 0,7 do
@@ -281,9 +233,6 @@ function onCreatePost()
                 for i = 0,7 do
                     setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                     setPropertyFromGroup('playerStrums',i,'downScroll',false)
-                end
-                for i = 8,8 do
-                    setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 250))
                 end
             end
             if mania == 17 then
@@ -303,30 +252,31 @@ function onCreatePost()
         if not downscroll then
             if mania == nil then
                 for i = 0,3 do
-                    if (math.round(i / 2)) == (i / 2) do
+                    if (math.round(i / 2)) == (i / 2) then
                         setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                         setPropertyFromGroup('playerStrums',i,'downScroll',true)
                     end
                 end
             else -- if mania has an int in it
                 for i = 0,mania do
-                    if (math.round(i / 2)) == (i / 2) do
+                    if (math.round(i / 2)) == (i / 2) then
                         setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') + 500))
                         setPropertyFromGroup('playerStrums',i,'downScroll',true)
                     end
                 end
+            end
             BarConfig = {706, 620}
         else -- if downscroll == true then
             if mania == nil then
                 for i = 0,3 do
-                    if (math.round(i / 2)) == (i / 2) do
+                    if (math.round(i / 2)) == (i / 2) then
                         setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                         setPropertyFromGroup('playerStrums',i,'downScroll',false)
                     end
                 end
             else
                 for i = 0,mania do
-                    if (math.round(i / 2)) == (i / 2) do
+                    if (math.round(i / 2)) == (i / 2) then
                         setPropertyFromGroup('playerStrums', i, 'y', (getPropertyFromGroup('strumLineNotes', i, 'y') - 500))
                         setPropertyFromGroup('playerStrums',i,'downScroll',false)
                     end
