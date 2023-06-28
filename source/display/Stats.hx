9137fdbcac56b9a634b38b26c3e366998e6a1dbe
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2023-06-28 19:19:08 -0400

    Graph Statistics
    Tryna make it togglable using F9

--- source/display/Stats.hx
+++ source/display/Stats.hx
@@ -1,46 +1,148 @@
+// Taken from https://github.com/yupswing/plik/blob/master/com/akifox/plik/debug/Performance.hx
+
 package display;
 
+import haxe.Timer;
+import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.display.DisplayObject;
+import openfl.display.Shape;
+import openfl.display.BitmapData;
+import openfl.display.Bitmap;
+import openfl.geom.Rectangle;
 import openfl.events.Event;
+import openfl.system.System;
+import openfl.text.TextField;
+import openfl.text.Font;
+import openfl.text.TextFormat;
+import openfl.Assets;
 import openfl.Lib;
 import flixel.FlxG;
-import flixel.FlxSprite;
 import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.util.FlxColor;
+
+/**
+ * Performance.hx
+ * Haxe/OpenFL class to display memory usage.
+ *
+ * More informations here
+ * https://github.com/yupswing/plik/tree/master/com/akifox/plik/debug
+ *
+ * @author Simone Cingano (yupswing) PLIK Studio
+ * @licence MIT
+ *
+ * Based on this post written by Kirill Poletaev
+ * http://haxecoder.com/post.php?id=24
+ */
 
 class Stats extends Sprite
 {
-    // Graph stuff
-    var framesSprite:Sprite;
-    var memSprite:Sprite;
-    var graphColor:Int;
+    private var fpsText:TextField;
 
-    // Info stuff
-    var fps:Float = 0;
-    var mem:Float = 0;
+    private var fpsHistory:Array<Int>;
+    private var fpsHistoryLen:Int = 180;
+    private var skipped = 0;
+    private var skip = 10;
+	private var times:Array<Float>;
 
-    public function new(x:Float = 6, y:Float = 6, color:Int = 0xFFFFFFFF)
-    {
-        super();
-        this.x = x;
-        this.y = y;
-        graphColor = color;
-        addEventListener(Event.ENTER_FRAME, (e:Event) -> enterFrame(Sys.time()));
-    }
+	private var padding:Int = 10;
+	private var paddingY:Int = 10;
 
-    function enterFrame(deltaTime:Float) {
-        updateFPS(deltaTime);
-        updateMemory(deltaTime);
-    }
+	public var graphBarTickness = 3;
+	public var graphBarPadding = 1;
+	private var barHeight = 30;
 
-    ////////////
-    function updateFPS(deltaTime:Float) {
-        fps = FPSCounter.frameRate;
-        //trace(fps);
-    }
+	private var memPeak:Float = 0;
 
-    function updateMemory(deltaTime:Float) {
-        mem = FPSCounter.memory;
-        //trace(mem);
-    }
+    private var bound:Bitmap;
+    private var graph:Shape;
+    private var boundData:BitmapData;
+    private var logo:Bitmap;
+
+    private var fullHeight:Int = 50;
+
+	public function new(x, y, font:String) 
+	{
+		super();
+
+		this.x = x;
+		this.y = y;
+
+		// Setup arrays
+		fpsHistory = [];
+		for (i in 0...fpsHistoryLen) fpsHistory.push(0);
+		times = [];
+
+		/// ###################################
+
+		/// ### PERFORMANCE TEXT
+    	fpsText = new TextField();
+        fpsText.x = FlxG.width - (fpsText.width + 2);
+		fpsText.selectable = false;
+		fpsText.defaultTextFormat = new TextFormat(font, 16, 0x66FFFFFF);
+		fpsText.text = "FPS: 0";
+		fpsText.embedFonts = true;
+
+		fullHeight = Std.int(fpsText.textHeight + paddingY*3);
+		barHeight = Std.int(fpsText.textHeight)-paddingY;
+
+		/// ###################################
+
+        var nextX:Float = padding;
+
+		/// ### GRAPH
+        graph = new Shape();
+        graph.x = FlxG.width - 190;
+        graph.y = paddingY;
+        nextX = graph.x + (graphBarTickness+graphBarPadding)*(fpsHistoryLen-2) + graphBarTickness + padding;
+    	fpsText.y = paddingY + 18;
+
+		/// ###################################
+
+		bound = new Bitmap();
+        bound.x = graph.x - 7;
+		onResize(null);
+		addChild(bound);
+		addChild(graph);
+		addChild(fpsText);
+		
+		/// ###################################
+
+		Lib.current.stage.addEventListener(Event.ENTER_FRAME, onEnter);
+		Lib.current.stage.addEventListener(Event.RESIZE, onResize);
+	}
+
+    var lerpFramerate:Float;
+	private function onEnter(_):Void
+	{
+        if (visible) {
+            lerpFramerate = FlxMath.lerp(lerpFramerate, FPSCounter.frameRate, 0.325);
+            drawGraph(Std.int(lerpFramerate+FlxG.elapsed));
+            fpsText.text = "FPS: " + Std.int(lerpFramerate+FlxG.elapsed) + ' (' + FPSCounter.frameTime + ' ms)';
+            fpsText.textColor = FlxColor.interpolate(0xFFFFFFFF, 0xFFFF0000, FlxEase.quadOut(50));
+            fpsText.autoSize = RIGHT;
+        }
+	}
+
+    var graphSpeed:Float = 0.02;
+	private function drawGraph(fps:Int):Void 
+	{
+		var color:Int;
+		fpsHistory.push(fps);
+        fpsHistory.shift();
+        graph.graphics.clear();
+        for (i in 0...fpsHistoryLen) {
+        	graph.graphics.moveTo(i+i*graphSpeed,barHeight-2*fpsHistory[i]/ClientPrefs.framerate*3);
+        	color = 0xFF00FFFF;
+        	graph.graphics.lineStyle(4, color);
+        	graph.graphics.lineTo(i+i*graphSpeed,barHeight-2*fpsHistory[i]/ClientPrefs.framerate*3);
+        }
+	}
+
+	private function onResize(_):Void
+	{
+		boundData = new BitmapData(Lib.current.stage.stageWidth,fullHeight);
+		boundData.fillRect(new Rectangle(0,0,Lib.current.stage.stageWidth,fullHeight),0x88000000);
+		bound.bitmapData = boundData;
+	}	
 }
