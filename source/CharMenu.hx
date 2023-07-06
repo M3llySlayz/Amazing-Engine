a7feffb3b2b47b04f260f6f8214533bdedc1bd03
M3llySlayz <matt2nou4u@gmail.com>
2023-07-06 15:33:26 -0400

    Formatting

--- source/CharMenu.hx
+++ source/CharMenu.hx
@@ -284,10 +284,7 @@ class CharMenu extends MusicBeatState
 
 				new FlxTimer().start(1, function(tmr:FlxTimer)
 				{
-					PlayState.SONG = Song.loadFromJson(PlayState.SONG.song+CoolUtil.getDifficultyFilePath(), PlayState.SONG.song);
 					if (unlockedCharacters[curSelected] != PlayState.SONG.player1) PlayState.SONG.player1 = daSelected;
-					PlayState.storyDifficulty = FreeplayState.curDifficulty;
-					PlayState.isStoryMode = false;
 					LoadingState.loadAndSwitchState(new PlayState());
 				});
 			}
@@ -510,16 +507,18 @@ class CharMenu extends MusicBeatState
 	}
 
 	function cacheTheGuy(newCharacter:String) {
-		if (boyfriendMap != null)
+		if (boyfriendMap != null) {
 			if(!boyfriendMap.exists(newCharacter)) {
 				try{
-				var newBoyfriend:Boyfriend = new Boyfriend(0, 0, newCharacter);
-				boyfriendMap.set(newCharacter, newBoyfriend);
-				boyfriendGroup.add(newBoyfriend);
-				newBoyfriend.alpha = 0.00001;
-				} catch(e){
+					var newBoyfriend:Boyfriend = new Boyfriend(0, 0, newCharacter);
+					boyfriendMap.set(newCharacter, newBoyfriend);
+					boyfriendGroup.add(newBoyfriend);
+					newBoyfriend.alpha = 0.00001;
+				} 
+				catch(e) {
 					trace('Probably another fucking null object reference');
 				}
 			}
+		}
 	}
 }
