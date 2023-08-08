860c7550e52bc3db8295429eb838726eacf9f069
SomeGuyWhoLovesCoding <99929173+SomeGuyWhoLovesCoding@users.noreply.github.com>
2023-08-08 09:34:27 -0400

    Update LongNotes.lua

--- assets/preload/scripts/LongNotes.lua
+++ assets/preload/scripts/LongNotes.lua
@@ -16,15 +16,21 @@ function goodNoteHit(id, direction, noteType, isSustainNote)
 		if isSustainNote then
 			-- Fix for play as opponent
 			if cpuPlay then
-				if getProperty('dad.animation.curAnim.curFrame')>4 then setProperty('dad.animation.curAnim.curFrame', 2) end
-				setProperty('dad.holdTimer', 0);
-			end
-			if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
-				if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
-				setProperty('gf.holdTimer', 0);
+				if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
+					if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
+					setProperty('gf.holdTimer', 0);
+				else
+					if getProperty('dad.animation.curAnim.curFrame')>4 then setProperty('dad.animation.curAnim.curFrame', 2) end
+					setProperty('dad.holdTimer', 0);
+				end
 			else
-				if getProperty('boyfriend.animation.curAnim.curFrame')>4 then setProperty('boyfriend.animation.curAnim.curFrame', 2) end
-				setProperty('boyfriend.holdTimer', 0);
+				if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
+					if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
+					setProperty('gf.holdTimer', 0);
+				else
+					if getProperty('boyfriend.animation.curAnim.curFrame')>4 then setProperty('boyfriend.animation.curAnim.curFrame', 2) end
+					setProperty('boyfriend.holdTimer', 0);
+				end
 			end
 		end
 	end
@@ -35,15 +41,21 @@ function opponentNoteHit(id, direction, noteType, isSustainNote)
 		if isSustainNote then
 			-- Fix for play as opponent
 			if cpuPlay then
-				if getProperty('boyfriend.animation.curAnim.curFrame')>4 then setProperty('boyfriend.animation.curAnim.curFrame', 2) end
-				setProperty('boyfriend.holdTimer', 0);
-			end
-			if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
-				if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
-				setProperty('gf.holdTimer', 0);
+				if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
+					if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
+					setProperty('gf.holdTimer', 0);
+				else
+					if getProperty('boyfriend.animation.curAnim.curFrame')>4 then setProperty('boyfriend.animation.curAnim.curFrame', 2) end
+					setProperty('boyfriend.holdTimer', 0);
+				end
 			else
-				if getProperty('dad.animation.curAnim.curFrame')>4 then setProperty('dad.animation.curAnim.curFrame', 2) end
-				setProperty('dad.holdTimer', 0);
+				if getPropertyFromGroup('notes', id, 'gfNote') or noteType == 'GF Sing' then
+					if getProperty('gf.animation.curAnim.curFrame')>4 then setProperty('gf.animation.curAnim.curFrame', 2) end
+					setProperty('gf.holdTimer', 0);
+				else
+					if getProperty('dad.animation.curAnim.curFrame')>4 then setProperty('dad.animation.curAnim.curFrame', 2) end
+					setProperty('dad.holdTimer', 0);
+				end
 			end
 		end
 	end
