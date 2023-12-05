d34150195552989192aab32d0c98fc68d219cf2e
IrshaadAli1 <irshaad.k.ali@gmail.com>
2023-12-05 18:24:46 -0400

    still barely alive lmao

---
+++ source/Tasks.hx
@@ -0,0 +1,25 @@
+/*
+    THIS FILE IS NOT A FILE USED FOR THE ENGINE
+    THIS IS MERELY USED FOR NOTING TASKS DOWN
+
+    Note for Irshaad (05/12/2023 {DD/MM/YYYY}):
+        Please don't forget to do this.
+		It's the AE plan we've been talking about for over a month now.
+		Just do it already.
+
+        - Events:
+            - Make the ability to put zero, one, two and more inputs in an event
+        - Characters:
+            - Rewrite how Character JSONS are read
+            - Add Character Indexes (Helps for more than one alt animation)
+            - Add Character Index Names (Just an alternative method)
+            - Make Event specific for this
+            - Keep AE's script of reading Psych (and probably Leather)'s Char Json files
+            - Still have AE have the ability to read an AE Json File
+            - Let AE have the ability to still write a Char JSON file like a Psych Char Json file
+            - Add the ability to write an AE Char JSON file
+        - Char Editor:
+            - Add a scroll for the list of anims
+            - Add the index name anims
+            - Refreshen the thing up a bit wouldja'?
+*/
